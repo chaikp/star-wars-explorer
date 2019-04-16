f342a3cd1508087af770dd3eff93f9be0ef8b60f
ChaiKP <kinnpong.chai@gmail.com>
2019-04-16 15:11:44 +0800

    retrieve full data from API

--- src/action.js
+++ src/action.js
@@ -55,7 +55,7 @@ export const reloadDetailsModal = (data, dataClass) => {
 export const requestCharacters = () => (dispatch) => {
 
   dispatch({type: REQUEST_CHARACTER_PENDING});
-  /*
+  
   // SWAPI only returns up to 10 items per call,
   //  require recursive calls to get all objects
   let dataArray = []
@@ -74,18 +74,18 @@ export const requestCharacters = () => (dispatch) => {
   }
 
   fetchCharacters('https://swapi.co/api/people')
-  */
+  /*
   fetch('https://swapi.co/api/people')
       .then(response => response.json())
       .then(data => dispatch({type: REQUEST_CHARACTER_SUCCESS, payload: data}))
       .catch(error => dispatch({type: REQUEST_CHARACTER_FAILED, payload: error}))
-  
+  */
 }
 
 export const requestPlanets = () => (dispatch) => {
   
   dispatch({type: REQUEST_PLANETS_PENDING});
-  /*
+  
   // SWAPI only returns up to 10 items per call,
   //  require recursive calls to get all objects
   let dataArray = []
@@ -104,18 +104,18 @@ export const requestPlanets = () => (dispatch) => {
   }
 
   fetchPlanets('https://swapi.co/api/planets')
-  */
+  /*
   fetch('https://swapi.co/api/planets')
       .then(response => response.json())
       .then(data => dispatch({type: REQUEST_PLANETS_SUCCESS, payload: data}))
       .catch(error => dispatch({type: REQUEST_PLANETS_FAILED, payload: error}))
-  
+  */
 }
 
 export const requestSpecies = () => (dispatch) => {
 
   dispatch({type: REQUEST_SPECIES_PENDING});
-  /*
+  
   // SWAPI only returns up to 10 items per call,
   //  require recursive calls to get all objects
   let dataArray = []
@@ -134,19 +134,19 @@ export const requestSpecies = () => (dispatch) => {
   }
 
   fetchSpecies('https://swapi.co/api/species')
-  */
+  /*
   //fetch('https://swapi.co/api/species')
   fetch('https://swapi.co/api/species/?page=4')
       .then(response => response.json())
       .then(data => dispatch({type: REQUEST_SPECIES_SUCCESS, payload: data}))
       .catch(error => dispatch({type: REQUEST_SPECIES_FAILED, payload: error}))
-  
+  */
 }
 
 export const requestFilms = () => (dispatch) => {
   
   dispatch({type: REQUEST_FILMS_PENDING});
-  /*
+  
   // SWAPI only returns up to 10 items per call,
   //  require recursive calls to get all objects
   let dataArray = []
@@ -164,29 +164,29 @@ export const requestFilms = () => (dispatch) => {
       })
   }
 
-  fetchPlanets('https://swapi.co/api/films')
-  */
+  fetchFilms('https://swapi.co/api/films')
+  /*
   fetch('https://swapi.co/api/films')
       .then(response => response.json())
       .then(data => dispatch({type: REQUEST_FILMS_SUCCESS, payload: data}))
       .catch(error => dispatch({type: REQUEST_FILMS_FAILED, payload: error}))
-  
+  */
 }
 
 export const requestStarships = () => (dispatch) => {
   
   dispatch({type: REQUEST_STARSHIPS_PENDING});
-  /*
+  
   // SWAPI only returns up to 10 items per call,
   //  require recursive calls to get all objects
   let dataArray = []
-  const fetchFilms = url => {
+  const fetchStarships = url => {
     fetch(url)
       .then(response => response.json())
       .then(data => {
         dataArray = dataArray.concat(data.results)   
         data.next !== null ? 
-          fetchFilms(data.next) : 
+          fetchStarships(data.next) : 
           dispatch({type: REQUEST_STARSHIPS_SUCCESS, payload: {results: dataArray}}); 
       })
       .catch(error => {
@@ -194,29 +194,29 @@ export const requestStarships = () => (dispatch) => {
       })
   }
 
-  fetchPlanets('https://swapi.co/api/starships')
-  */
+  fetchStarships('https://swapi.co/api/starships')
+  /*
   fetch('https://swapi.co/api/starships')
       .then(response => response.json())
       .then(data => dispatch({type: REQUEST_STARSHIPS_SUCCESS, payload: data}))
       .catch(error => dispatch({type: REQUEST_STARSHIPS_FAILED, payload: error}))
-  
+  */
 }
 
 export const requestVehicles = () => (dispatch) => {
   
   dispatch({type: REQUEST_VEHICLES_PENDING});
-  /*
+  
   // SWAPI only returns up to 10 items per call,
   //  require recursive calls to get all objects
   let dataArray = []
-  const fetchFilms = url => {
+  const fetchVehicles = url => {
     fetch(url)
       .then(response => response.json())
       .then(data => {
         dataArray = dataArray.concat(data.results)   
         data.next !== null ? 
-          fetchFilms(data.next) : 
+          fetchVehicles(data.next) : 
           dispatch({type: REQUEST_VEHICLES_SUCCESS, payload: {results: dataArray}}); 
       })
       .catch(error => {
@@ -224,11 +224,11 @@ export const requestVehicles = () => (dispatch) => {
       })
   }
 
-  fetchPlanets('https://swapi.co/api/vehicles')
-  */
+  fetchVehicles('https://swapi.co/api/vehicles')
+  /*
   fetch('https://swapi.co/api/vehicles')
       .then(response => response.json())
       .then(data => dispatch({type: REQUEST_VEHICLES_SUCCESS, payload: data}))
       .catch(error => dispatch({type: REQUEST_VEHICLES_FAILED, payload: error}))
-  
+  */
 }
